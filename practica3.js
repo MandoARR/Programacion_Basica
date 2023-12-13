13f409643fea6f61fa3dc6b1897961a1acaa33cc
MandoARR <150207139+MandoARR@users.noreply.github.com>
2023-12-12 20:52:05 -0600

    Practicas 3&4

---
+++ practica3.js
@@ -0,0 +1,15 @@
+
+
+//Escribe un codigo para calcular el area de un circulo
+
+let radio = 4
+const pi = 3.1416
+
+let area = pi * (radio**2)
+
+//radio = 4
+
+// a = pi * r2 
+
+//Mostrar el area
+console.log("Area: ", area)
---
+++ practica4.js
@@ -0,0 +1,29 @@
+//Control de flujo
+
+//Determina si un numero es par o impar
+//2 4 6 8 10 pares / 2 = residuo == 0
+let num = 12
+let residuo = num % 2
+
+//SI HAY RESIDUO
+if(residuo == 0)
+{
+    console.log("PAR")
+} else 
+{
+    console.log("ES IMAPR")
+}
+
+
+/*
+1
+2
+3
+4 console.log("ES PAR")
+5
+6 console.log("ES IMPAR")
+7
+8
+9
+10
+*/
