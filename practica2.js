62165a39219ff66d94ec88709342a7ce278e416c
MandoARR <150207139+MandoARR@users.noreply.github.com>
2023-12-12 20:51:53 -0600

    Sugerencias de Roberto

--- practica2.js
+++ practica2.js
@@ -16,8 +16,15 @@ let division = num1 / num2
 let multiplicacion = num1 * num2
 
 //Imprimir todos los resultados
+
+/*
 console.log("Resultado de SUMA 4+2: ",suma)
 console.log("Resultado de RESTA 4-2: ",resta)
 console.log("Resultado de DIVISION 4/2: ",division)
 console.log("Resultado de MULTIPLICACION 4*2: ",multiplicacion)
+*/
 
+console.log("Resultado de SUMA ", num1,"+",num2, "=",suma)
+console.log("Resultado de RESTA ", num1,"+",num2, "=",resta)
+console.log("Resultado de DIVISION ", num1,"+",num2, "=",division)
+console.log("Resultado de MULTIPLICACION ", num1,"+",num2, "=",multiplicacion)
