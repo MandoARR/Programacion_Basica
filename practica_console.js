e89c3b8e5561c553ceb93f9403760ab5ffbf61b7
MandoARR <150207139+MandoARR@users.noreply.github.com>
2023-12-12 19:58:27 -0600

    Ejemplos de console

---
+++ practica_console.js
@@ -0,0 +1,21 @@
+console.log("HOLA MUNDO")
+
+console.group("GRUPO 1 : CONTANDO")
+
+console.count("Contador")
+console.count("Contador")
+console.count("Contador")
+
+console.group("GRUPO 2 : TIPOS DE MENSAJE")
+
+console.warn("Advertencia")
+console.error("Mensaje de error")
+
+console.group("GRUPO 3 : CON ESTILO")
+
+console.log("Text con estilo","color: red; font-size: 18px;")
+
+console.group("GRUPO 4 : CON VARIABLES")
+
+let nombre = "Armando"
+console.log("Nombre: ",nombre)
