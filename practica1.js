58dc2039006be504cdd8ea4250104b52191b96f9
MandoARR <150207139+MandoARR@users.noreply.github.com>
2023-12-12 20:18:47 -0600

    Practica 1&2

---
+++ practica1.js
@@ -0,0 +1,25 @@
+//Ejemplos de comentarios en linea
+
+//console.log("No se mostrara porque esta en un comentario")
+
+/*
+Est es un comentario
+de
+bloque
+*/
+
+//Aqui se declara la variable que guarda el nombre del usuario
+let nombre
+
+//Aqui se declara y se asigna la variable
+let edad = 24
+
+//Aqui estoy asignando el valor del nombre
+nombre="Armando"
+
+//Aqui muestro el valor del nombre
+console.log("Nombre actual: ",nombre)
+
+//En este punto el valor de la variable de Armando a otro valor
+nombre="Ruben"
+console.log("Nombre actual: ",nombre, "| Edad: ", edad)
---
+++ practica2.js
@@ -0,0 +1,23 @@
+//Definir 2 numeros
+let num1 = 4
+let num2 = 2
+
+//operaciones Basicas
+//+
+let suma = num1 + num2
+
+//-
+let resta = num1 - num2
+
+// /
+let division = num1 / num2
+
+// *
+let multiplicacion = num1 * num2
+
+//Imprimir todos los resultados
+console.log("Resultado de SUMA 4+2: ",suma)
+console.log("Resultado de RESTA 4-2: ",resta)
+console.log("Resultado de DIVISION 4/2: ",division)
+console.log("Resultado de MULTIPLICACION 4*2: ",multiplicacion)
+
