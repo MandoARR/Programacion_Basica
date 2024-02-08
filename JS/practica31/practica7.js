4b1a19003d3b2dd528a8e6f45b56509fd2a8f1c9
MandoARR <150207139+MandoARR@users.noreply.github.com>
2024-02-08 16:39:19 -0600

    ejercicio 7 completo

--- JS/practica31/practica7.js
+++ JS/practica31/practica7.js
@@ -1,5 +1,11 @@
 //Cambia los primero 3 elementos y ponlos al final
 caballeros = ["Seiya", "Shiryu", "Hyōga", "Shun", "Ikki", "Saori", "Tenma"] 
 
-newArray = caballeros.slice(0,3)
+firstKnigths = caballeros.slice(0,3)
+caballeros.shift()
+caballeros.shift()
+caballeros.shift()
+
+caballeros = caballeros.concat(firstKnigths) 
+console.log(caballeros)
 
--- JS/practica31/practica8.js
+++ JS/practica31/practica8.js
@@ -1,4 +1,11 @@
+//Dame las palabras con más coincidencias en el length
 caballeros = ["Seiya", "Shiryu", "Hyōga", "Shun", "Ikki", "Saori", "Tenma"] 
 
+let Bigknigth = []
+caballeros.forEach(element => {
+    if (Bigknigth.length < element.length){
+        Bigknigth = element
+    }
+})
 
-
+console.log(Bigknigth.length)
