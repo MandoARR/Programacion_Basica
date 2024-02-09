a4f377a1349fe33dc6f23c1587515d86a78e0007
MandoARR <150207139+MandoARR@users.noreply.github.com>
2024-02-08 21:23:06 -0600

    clase JSON 08-02-24

--- JS/practica40.js
+++
@@ -1,7 +0,0 @@
-let caballeros = ["Seiya", "Shiryu", "Hyōga", "Shun", "Ikki", "Saori", "Tenma"];
-
-caballerosOrder = caballeros.sort().join("!").split
-
-console.log(caballeros.sort())
-
-
---
+++ JSON/ejemplo3.js
@@ -0,0 +1,71 @@
+const array = [
+    {
+      nombre: "Juan",
+      edad: 30,
+      activo: true,
+      intereses: ["programacion", "viajes", "lectura"],
+      direccion: {
+        calle: "Calle Principal",
+        numero: 123,
+        ciudad: "Ciudad Principal"
+      }
+    },
+    {
+      nombre: "María",
+      edad: 25,
+      activo: false,
+      intereses: ["música", "deportes", "arte"],
+      direccion: {
+        calle: "Avenida Central",
+        numero: 456,
+        ciudad: "Ciudad Secundaria"
+      }
+    },
+    {
+      nombre: "Pedro",
+      edad: 35,
+      activo: true,
+      intereses: ["fotografí­a", "naturaleza", "cocina"],
+      direccion: {
+        calle: "Calle de los Arboles",
+        numero: 789,
+        ciudad: "Ciudad Terciaria"
+      }
+    },
+    {
+      nombre: "Ana",
+      edad: 28,
+      activo: true,
+      intereses: ["teatro", "moda", "viajes"],
+      direccion: {
+        calle: "Avenida de las Flores",
+        numero: 1011,
+        ciudad: "Ciudad Cuarta"
+      }
+    },
+    {
+      nombre: "Luis",
+      edad: 40,
+      activo: false,
+      intereses: ["ciencia", "historia", "tecnologí­a"],
+      direccion: {
+        calle: "Avenida del Saber",
+        numero: 1213,
+        ciudad: "Ciudad Quinta"
+      }
+    },
+    {
+      nombre: "Teresa",
+      edad: 15,
+      activo: false,
+      intereses: ["historia", "natacion", "tecnologí­a"],
+      direccion: {
+        calle: "Avenida del Conocimiento",
+        numero: 1213,
+        ciudad: "Ciudad Quinta"
+      }
+    },
+  ];
+
+  const hayUsuarioActivo = array.some(usuario => usuario.activo);
+  console.log(hayUsuarioActivo);
