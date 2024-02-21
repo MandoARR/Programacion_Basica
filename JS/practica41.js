ee7bf05b619f2bfcefaf3d7580683f7d1a0400db
MandoARR <armando_arr@outlook.com>
2024-02-20 20:00:49 -0600

    ejercicios Hash Map

--- JS/practica41.js
+++ JS/practica41.js
@@ -1,4 +1,7 @@
+<<<<<<< Updated upstream
 //EJERCICIO REDUCE()
+=======
+>>>>>>> Stashed changes
 //Calcula la suma total de los precios de los productos
 const productos = [
     { nombre: 'Camiseta', precio: 20 },
@@ -7,6 +10,18 @@ const productos = [
     { nombre: 'Sombrero', precio: 15 }
 ];
 
+<<<<<<< Updated upstream
 const sum = productos.reduce((acc,producto) => acc + producto.precio,0)
 
 console.log(sum)
+=======
+
+const expensive = productos.reduce((acc, producto) => {
+    if (acc.precio > producto.precio) {
+        return acc;
+    }
+        return producto;
+},{});
+
+console.log("El producto más caro es:", expensive.nombre, " - $", expensive.precio);
+>>>>>>> Stashed changes
--- JS/practica44.js
+++ JS/practica44.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 //EJERCICIO SET()
 //Encuentra los elementos comunes entre dos arreglos utilizando un Set y el método filter().
 const arreglo1 = [1, 2, 3, 4, 5];
@@ -8,3 +9,22 @@ const newArray2 = new Set(arreglo2)
 const arrayUnique = arreglo1.filter(element => newArray2.has(element))
 
 console.log(arrayUnique)
+=======
+//Calcula la suma total de los precios de los productos
+const productos = [
+    { nombre: 'Camiseta', precio: 20 },
+    { nombre: 'Pantalón', precio: 40 },
+    { nombre: 'Zapatos', precio: 50 },
+    { nombre: 'Sombrero', precio: 15 }
+];
+
+
+const expensive = productos.reduce((acc, producto) => {
+    if (acc.precio > producto.precio) {
+        return acc;
+    }
+        return producto;
+},{});
+
+console.log("El producto más caro es:", expensive.nombre, " - $", expensive.precio);
+>>>>>>> Stashed changes
