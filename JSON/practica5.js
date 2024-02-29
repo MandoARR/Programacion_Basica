56a5e6e387d2e0bd8c293bba88015152f021e1ed
MandoARR <150207139+MandoARR@users.noreply.github.com>
2024-02-29 16:47:55 -0600

    ejercicios de tarea atrasados
    Los ejercicios que no se realizaron del dia 22-Feb-2024 por no estar presente en la clase se realizaron hasta hoy

--- JSON/practica5.js
+++ JSON/practica5.js
@@ -1,8 +1,11 @@
 // Crea un método que reciba el nombre de un pokemon y traiga sus datos.
-const url = "https://pokeapi.co/api/v2/pokemon/6"; 
-fetch(url) 
-.then((response) => response.json()) 
-.then((data) => { 
-console.log(data)
-})
+const url = "https://pokeapi.co/api/v2/pokemon/";
+let pokemon = "garchomp"
 
+function pokeApi(){
+     fetch(url+pokemon).then((response) => response.json()).then(data => {
+        console.log(data)
+    }) 
+}
+
+pokeApi();
--- JSON/practica6/pokeApi.js
+++ JSON/practica6/pokeApi.js
@@ -1,22 +1,14 @@
 //Haz una tabla con los datos que vienen aquí, algo como el ejemplo, fíjate que al final del URL
 //viene el nombre.
 
-const url = "https://pokeapi.co/api/v2/pokemon/"; 
-fetch(url) 
-.then((response) => response.json()) 
-.then((data) => { 
-orderPokemon2(data.results)
-})
+const url = "https://pokeapi.co/api/v2/pokemon/";
 
-function orderPokemon(pokemons){
-    pokemons.sort((a,b) => a.name.localeCompare(b.name)) //localeCompare hace una comparacion con Strings
-    pokemons.forEach(element => {
-        console.log(element.name)
-    });
+function pokeApi(){
+     fetch(url).then((response) => response.json()).then(data => {
+        const results = data.results
+
+        
+    }) 
 }
 
-function orderPokemon2(pokemons){
-    const newArray = pokemons.map((element) => element.name);
-    newArray.sort()
-    console.log(newArray)
-}
+pokeApi();
