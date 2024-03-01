48005db7fb6ad23621fcfcafc6e934ef32f916c5
MandoARR <armando_arr@outlook.com>
2024-02-29 22:37:59 -0600

    Ejercicio Tabla Pokemon HTML,CSS,JS

--- JSON/practica6/pokeApi.js
+++ JSON/practica6/pokeApi.js
@@ -1,14 +1,21 @@
 //Haz una tabla con los datos que vienen aquí, algo como el ejemplo, fíjate que al final del URL
 //viene el nombre.
-
 const url = "https://pokeapi.co/api/v2/pokemon/";
 
 function pokeApi(){
      fetch(url).then((response) => response.json()).then(data => {
-        const results = data.results
-
-        
+        //console.log(data)
+        tabla(data)
     }) 
+}
+
+function tabla(list){
+    const results = list.results
+    const name_pokemon = results.map((element) => (element.name).toUpperCase()) 
+    const url_pokemon = results.map((element) => element.url)
+
+    document.getElementById('pokemons').innerHTML = "Pokémon: " + name_pokemon
+    document.getElementById('urls').innerHTML = "URL: " + url_pokemon
 }
 
 pokeApi();
