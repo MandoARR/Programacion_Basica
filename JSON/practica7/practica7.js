d1c218ed4892fcd87332335fd0b642d321617db6
MandoARR <armando_arr@outlook.com>
2024-03-05 22:21:41 -0600

    API

--- JSON/practica7/practica7.js
+++ JSON/practica7/practica7.js
@@ -1,25 +1,3 @@
-const myPromise = () => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            const probabilidad = Math.random() < 0.5;
-            if (probabilidad) {
-                resolve(["Pizza", "Música", "Videojuegos"]);
-            } else {
-                reject(["Aguacate", "Tráfico", "Facturas"]);
-            }
-        }, 2000);
-    });
-};
-
-myPromise()
-    .then((list) => {
-        document.getElementById('resultado').innerHTML = "Cosas que me gustan: " + list.join(", ");
-    })
-    .catch((list) => {
-        document.getElementById('resultado').innerHTML = "Cosas que NO me gustan: " + list.join(", ");
-    });
-
-
 
 //Math.random() < 0.5; // 50% de probabilidad de éxito
 //Crear una promesa que espere 2 segundos para contestar 
